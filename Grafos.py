d280fc889f5a8b3df1c165db1cfdedd1ec7bfe8a
Pedro Vitor <pedrovitor.silva14vs@gmail.com>
2023-12-13 23:52:20 -0300

    Grafo

--- Grafos.py
+++ Grafos.py
@@ -1,44 +1,43 @@
-import random
-import networkx as nx #Bibliotecas para manipulação de grafos
+import networkx as nx # Biblioteca para manipulação de grafos
 import matplotlib.pyplot as plt
+import random
 import math
+
 
 #Grafos Aleatórios:
 
-def grafoAl(x):
+def criar_grafo_aleatorio(x):
     p = math.log(x) / x
     arestas = []
 
     for i in range(1, x + 1):
         for j in range(1, x + 1):
-            if i < j:
+            if j > i:
                 t = random.uniform(0, 1)
                 if t > 1 - p:
                     arestas.append((i, j))
 
-    #Funções para criar um grafo não-direcionado e adicionar as arestas que estão na lista:
-    G = nx.Graph()
-    G.add_edges_from(arestas)
-
-    #Definição de layout(existem outros tipos):
-    layout = nx.spring_layout(G)  
-
+    # Funções para criar um grafo não-direcionado e adicionar as arestas que estão na lista:
+    grafo = nx.Graph()
+    grafo.add_edges_from(arestas)
+    
+    # Definição de layout(existem outros tipos):
+    pos = nx.spring_layout(grafo)
+    
     plt.figure(1)
     plt.title('Grafo Aleatório')
-    nx.draw(G, pos=layout, with_labels=True)
+    nx.draw(grafo, pos, with_labels=True)
     plt.show()
 
-    diametro = nx.diameter(G)
-    return G, diametro
-
-n = 100
-grafo_al, diametro_al = grafoAl(n)
+    # Calcular o diâmetro:
+    diametro = nx.diameter(grafo)
+    print(f"Diâmetro do Grafo Aleatório: {diametro}")
 
 
 
 # Grafos Heterogêneos:
 
-def grafoHet(x):
+def criar_grafo_heterogeneo(x):
     arestas = []
     L = [1, 2]
     arestas.append((1, 2))
@@ -48,21 +47,28 @@ def grafoHet(x):
         arestas.append((i, u))
         L.extend([u, i])
 
-    #Funções para cria um grafo não-direcionado e adicionar as arestas que estão na lista:
-    G = nx.Graph()
-    G.add_edges_from(arestas)
+    # Funções para criar um grafo não-direcionado e adicionar as arestas que estão na lista:
+    grafo = nx.Graph()
+    grafo.add_edges_from(arestas)
 
-    #Definição de layout(existem outros tipos):
-    layout = nx.kamada_kawai_layout(G)
-
+    # Definição de layout(existem outros tipos):
+    pos = nx.kamada_kawai_layout(grafo)
+    
     plt.figure(2)
-    plt.title('Grafo Heterogêneo')
-    nx.draw(G, pos=layout, with_labels=True)
+    plt.title("Grafo Heterogêneo")
+    nx.draw(grafo, pos, with_labels=True)
     plt.show()
 
-    diametro = nx.diameter(G)
-    return G, diametro
+    # Calcular Diâmetro:
+    diametro = nx.diameter(grafo)
+    print(f"Diâmetro do Grafo Heterogêneo: {diametro}")
 
-n = 100
-grafo_het, diametro_het = grafoHet(n)
+# Código principal que se inicia com redes de tamanho igual a 100
+
+c = 100
+    
+criar_grafo_aleatorio(c)
+criar_grafo_heterogeneo(c)
+
+
 
